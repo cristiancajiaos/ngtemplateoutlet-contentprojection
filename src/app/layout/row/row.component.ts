8a981269c8ff669fd4c16650f00434a8c697afd4
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-04-25 12:49:49 -0400

    Definido layout de componentes a mostrar

--- src/app/layout/row/row.component.ts
+++ src/app/layout/row/row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-row',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RowComponent implements OnInit {
 
+  @Input() item: string;
+  
   constructor() { }
 
   ngOnInit(): void {
